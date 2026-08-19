73a22430e910bd362a3aa7efb6e5819d7ceb1600
Soap0816 <gamor9947@gmail.com>
2026-08-19 00:58:11 -0400

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -1,5 +1,6 @@
 import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "./build/server";
+import type { Env } from "./app/lib/env.server";
 
 const handleRequest = createRequestHandler(build, "production");
 
